28b4cfad1c0f1b28d350f7ced636313d61b0441b
Scott Campbell <stc9486@gmail.com>
2020-04-09 12:48:08 -0700

    random

---
+++ methods/random.js
@@ -0,0 +1,10 @@
+const array = [4, 3, 9, 6, 23];
+const divByTwo = {};
+// ADD CODE HERE
+
+//write a for loop to iterate through array
+for(let i = 0; i < array.length; i++) {
+  let num = array[i];
+  //write a conditional that adds an item of array to the obj divByTwo IFF that item % 2 === 0
+  if(num % 2 === 0) divByTwo[num] = i
+}
