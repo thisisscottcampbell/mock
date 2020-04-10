3bd0286e659915a891e54bdbc89841b5566de0c9
Scott Campbell <stc9486@gmail.com>
2020-04-10 14:12:24 -0700

    split

---
+++ methods/split.js
@@ -0,0 +1,9 @@
+//Take two arguments, both optional, (seperator, limit)
+// 1. seperator: determines the character to use for splitting the string. If left blank, the string will be returned in tact, but as an array
+//2. limit: an interger specifying the number of splits. Items after limit will not be included in array,
+
+import { stringLiteral } from "@babel/types";
+
+
+string.split("") // with no spaces --> ["s","t","r","i","n","g"]
+string.split() // --> ['string']
