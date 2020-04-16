ab0d1fdbb75f1dab9a158bef8726e82d5cbef5fd
Scott Campbell <stc9486@gmail.com>
2020-04-16 10:42:46 -0700

    object.values

---
+++ methods/Object.values().js
@@ -0,0 +1,11 @@
+// DEFINITION
+//Returns an array of a given object's own enumerable property values (i.e., value of the object's keys), in the same order as that provided by a for...in loop.
+
+const cats = {
+  Garfield: lazy,
+  CheshireCat: far-out,
+  Tigger: lit
+}
+
+console.log(Object.values(object1));
+//returns: [lazy, far-out, lit]
