69953f72b59cdc1852f845ee35844de8a639e46c
Scott Campbell <stc9486@gmail.com>
2020-04-27 10:10:00 -0700

    random

---
+++ methods/Math....js
@@ -0,0 +1,13 @@
+//Math.floor
+  //rounds num down to whole interger
+(Math.floor(1.5))
+  //--> 1
+(Math.floor(2.3))
+  // --> 2
+
+//Math.random
+(Math.random());
+  // --> [some random number between 0 - 1]
+
+  
+
