23a316e7a6956ee1b185572ebe7c0af12e6acd68
Scott Campbell <stc9486@gmail.com>
2020-04-27 10:15:52 -0700

    sign

--- methods/Math....js
+++ methods/Math....js
@@ -1,13 +1,28 @@
 //Math.floor
   //rounds num down to whole interger
-(Math.floor(1.5))
+(Math.floor(1.8))
   //--> 1
-(Math.floor(2.3))
-  // --> 2
+(Math.floor(3.3))
+  // --> 3
+
+//Math.ceil
+  //rounds num up to whole interger
+(Math.ceil(1.5))
+//--> 2
+(Math.ceil(3.3))
+// --> 4
+
 
 //Math.random
+  //generates random number
 (Math.random());
   // --> [some random number between 0 - 1]
+
+  
+//Math.sign
+  //returns 1 if number is positive; 0 if a num is zero; and -1 if a number is negative;
+(Math.sign(-5));
+  // --> -1
 
   
 
