da36a7788f048a56de68581b909337d3af76d2d6
Scott Campbell <stc9486@gmail.com>
2020-10-09 18:05:26 -0400

    some

--- methods/some.js
+++ methods/some.js
@@ -3,7 +3,7 @@
 //if the callback returns true for at least one single value, entire function returns true
 //The result of some will ALWAYS be a boolean
 
-let arr = [1,2,3]
+const arr = [1,2,3]
 
 arr.some(num => {
   return num < 2
