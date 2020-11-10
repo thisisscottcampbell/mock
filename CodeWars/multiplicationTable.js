90fa1147276df03696e8a73e30ee92d44154d085
Scott Campbell <stc9486@gmail.com>
2020-11-10 16:29:43 -0500

    cw

---
+++ CodeWars/multiplicationTable.js
@@ -0,0 +1,41 @@
+// Create a function that accepts dimensions, of Rows x Columns, as parameters in order to create a multiplication table sized according to the given dimensions. **The return value of the function must be an array, and the numbers must be Fixnums, NOT strings.
+
+// Example:
+
+// multiplication_table(3,3)
+
+// 1 2 3
+// 2 4 6
+// 3 6 9
+
+// -->[[1,2,3],[2,4,6],[3,6,9]]
+
+// Each value on the table should be equal to the value of multiplying the number in its first row times the number in its first column.
+
+function multiplicationTable(row, col, i = 1, tables = []){
+  
+    //Base Case
+    if (i > row) return tables;
+  
+    const currTable = [];
+    const currTableLength = col;
+    const currFactor = i;
+    
+    let currVal = 1;
+    
+     //Updates    
+    while (currVal <= currTableLength) {
+      
+      let pushVal = currVal * currFactor;
+      
+      currTable.push(pushVal);
+      currVal += 1
+    }
+    
+    const newTable = [...tables, currTable];
+    i += 1;
+           
+    //RECURSIVE CASE
+    return multiplicationTable(row, col, i, newTable)
+    
+    }
