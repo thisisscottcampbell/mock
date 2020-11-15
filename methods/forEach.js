0f69abbd0d78e0db2e3d6785e307f313cde86cee
Scott Campbell <stc9486@gmail.com>
2020-11-15 16:36:53 -0500

    forEach

--- methods/forEach.js
+++ methods/forEach.js
@@ -14,44 +14,31 @@ const doubledArr = [];
 
 arr.forEach((num) => {
   const doubledNum = num * 2;
-  
+
   doubledArr.push(doubledNum);
 });
 
 //console.log(doubledArr)--> RETURNS arr = [2, 4, 6, 8, 10]
 
-
-//double value
-doubleVals = (arr) => {
-  let newArr = []
-  arr.forEach(num => {
-    newArr.push(num *2)
-  })
-  return newArr;
-}
-
 //return evens
-onlyEvans = (arr) => {
+const onlyEvens = (arr) => {
   const newArr = [];
+
   arr.forEach(num => {
-    (num % 2 === 0){
-      newArr.push(num)
-    }
+    if (num % 2 === 0) newArr.push(num)
   })
+
   return newArr
 }
 
 //first and last char of each string in array
 showFirstAndLast = (arr) => {
   const newArr = [];
+  
   arr.forEach(str => {
       newArr.push(str[0] + str[str.length-1])
   });
+  
   return newArr;
 }
 
-//times a number by ten if the number in the array is greater than 4
-
-array.forEach((num, i, arr) => {
- return arr[i] = num < 5 ? num : num * 10
-})
