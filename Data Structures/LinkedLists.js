8fe38a81ad44b74bdadd0ad33adb7ac9e091824d
Scott Campbell <stc9486@gmail.com>
2020-11-18 18:18:29 -0500

    traversal helper

--- Data Structures/LinkedLists.js
+++ Data Structures/LinkedLists.js
@@ -27,9 +27,50 @@ class LinkedList {
         this.length += 1;
         return this
     }
+    printList() {
+        const valsArray = [];
+        let curr = this.head;
+        while (curr !== null) {
+            valsArray.push(curr.value)
+            curr = curr.next
+        }
+        return valsArray
+    } 
+    insert(insertValue, i) {
+        if (i  <  0) return this;
+        if (i > this.length) return this.append(insertValue);
+
+        const newNode = {
+            value: insertValue,
+            next: null
+        }
+
+        const preInsertNode = this.traverseToIndex(i - 1);
+        const insertNodeNext = preInsertNode.next;
+        preInsertNode.next = newNode;
+        newNode.next = insertNodeNext;
+        this.length +=  1;
+        return this;
+    }
+    traverseToIndex (i) {
+        let counter = 0;
+
+        let currNode = this.head;
+        while (counter < i) {
+            currNode = currNode.next;
+            counter += 1;
+        }
+        return currNode;
+    }
 }
 
 const myList = new LinkedList(10);
+
 myList.append(5);
 myList.prepend(7);
+console.log(myList.printList());
+myList.insert(99, 99);
+console.log(myList.printList());
+myList.insert(12, 2)
+console.log(myList.printList());
 console.log(myList)
