d897f32ef1c45a5b1901fdaac400883c9b81777b
Scott Campbell <stc9486@gmail.com>
2020-11-18 17:41:32 -0500

    append

--- Data Structures/LinkedLists.js
+++ Data Structures/LinkedLists.js
@@ -5,15 +5,20 @@ class LinkedList {
             next: null
         }
         this.tail =  this.head;
-        this.length = 1;
-
-        return this;
+        this.length = 1;   
     }
-
-    // append(val) {
-    //     const newNode
-    // }
+    append(appendVal) {
+        const newNode = {
+            value: appendVal,
+            next: null
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length += 1;
+        return this
+    }
 }
 
 const node = new LinkedList(10);
+node.append(5);
 console.log(node)
