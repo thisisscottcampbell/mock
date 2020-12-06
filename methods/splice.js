c961b243d2776a51ba85b31a098463ee98acc884
Scott Campbell <stc9486@gmail.com>
2020-12-06 15:57:30 -0500

    splice

---
+++ methods/splice.js
@@ -0,0 +1,7 @@
+const array = [1,2,3,4,5,6]
+
+console.log('i am array before splice', array)
+const splicedItem = array.splice(3,1)
+console.log('i am spliced element', splicedItem);
+console.log('XXXXXXXXXXX')
+console.log('i am array after splice', array)
