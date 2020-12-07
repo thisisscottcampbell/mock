a7a33ac30903e4fda67d541369e3e8852c1b74e0
Scott Campbell <stc9486@gmail.com>
2020-12-07 17:09:01 -0500

    splice

--- methods/splice.js
+++ methods/splice.js
@@ -1,7 +1,14 @@
+//CAN Take up to three inputs
+//array.splice(targetIdx, deleteHowMany, insertThis);
+//modifies array
+
 const array = [1,2,3,4,5,6]
 
-console.log('i am array before splice', array)
-const splicedItem = array.splice(3,1)
-console.log('i am spliced element', splicedItem);
-console.log('XXXXXXXXXXX')
-console.log('i am array after splice', array)
+//assign removed element to a label
+const splicedItem = array.splice(2,1, 'insert me')
+console.log(splicedItem);
+  //3
+
+//log array to see value after splice
+console.log(array)
+  //[1,2,'insert me',4,5,6]
