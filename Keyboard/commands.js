64b6390d6ea2b5c4380d7421ce0271526b325ec9
Scott Campbell <stc9486@gmail.com>
2020-12-24 15:24:16 -0500

    keyboard

---
+++ Keyboard/commands.js
@@ -0,0 +1,17 @@
+1. command + up
+//top left of document
+
+2. command + down
+//bottom right of document
+
+3. command + right
+// move to the end of the line
+
+4. command + left
+// move to the beginning of the line
+
+5. option + up
+// move the current line up one line
+
+6. option + down
+//move the current line down one line
