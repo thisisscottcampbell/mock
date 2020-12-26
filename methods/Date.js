ee30fb467a5e4172c7cb7ad9756adf1352047be9
Scott Campbell <stc9486@gmail.com>
2020-12-26 14:52:05 -0500

    date

--- methods/Date.js
+++ methods/Date.js
@@ -1,3 +1,8 @@
 let date = new Date().toUTCString();
+//Sat, 26 Dec 2020 19:49:57 GMT
 
 const renderDate = new Date().toUTCString().slice(0, 16);
+//Sat, 26 Dec 2020
+
+const noDayDate = new Date().toUTCString.slice(5, 16);
+//26 Dec 2020
