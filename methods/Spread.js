0deb416c88bc28f8c2a2fd749daea6c627c2ed0c
Scott Campbell <stc9486@gmail.com>
2021-01-07 14:23:47 -0500

    spread

--- methods/Spread.js
+++ methods/Spread.js
@@ -4,3 +4,7 @@ const dogs = ['pluto', 'snoopy', 'brian'];
 const catsAndDogs = [...cats, ...dogs];
 
 //['buddy', 'felix', 'garfield','pluto', 'snoopy', 'brian']
+
+const catsAndDogsAndBirds = [...catsAndDogs, 'woodstock'];
+
+// ['buddy', 'felix', 'garfield', 'pluto', 'snoopy', 'brian', 'woodstock']
