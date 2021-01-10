7fa4f76b6e7dfbf85e25aa84277d7e2ce46a5ee9
Scott Campbell <stc9486@gmail.com>
2021-01-10 16:40:09 -0500

    splice

--- methods/splice.js
+++ methods/splice.js
@@ -7,16 +7,34 @@
 //2. How many to delete
 //3. Relace deleted elements with...
 
+//With ONE argument
+//will split array at the element
+//and return new array from that element--inclusive
 
 
 
 const array = [1,2,3,4,5,6]
 
 //assign removed element to a label
-const splicedItem = array.splice(2,1, 'insert me')
-console.log(splicedItem);
-  //3
+const spliceReturnsArrayOfDeletedElements = array.splice(2,2, 'insert me')
+console.log(spliceReturnsArrayOfDeletedElements);
+  //[3,4]
 
 //log array to see value after splice
 console.log(array)
-  //[1,2,'insert me',4,5,6]
+  //[1,2,'insert me', 5,6]
+
+const arr = [1,2,3,4,5,6]
+const spliced = arr.splice(1,1);
+
+console.log(arr);
+//[1, 3, 4, 5, 6]
+console.log(spliced);
+//[2]
+
+const singleArgSplice = arr.splice(2);
+
+console.log(arr);
+//[1, 3]
+console.log(singleArgSplice);
+//[4, 5, 6]
