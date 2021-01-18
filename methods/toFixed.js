0d52e226c94025f66a71d2dc2b315b742095216d
Scott Campbell <stc9486@gmail.com>
2021-01-18 18:45:39 -0500

    toFixed

--- methods/toFixed.js
+++ methods/toFixed.js
@@ -1,3 +1,20 @@
-const price = 2;
-price.toFixed(2);
-//2.00
+//int.toFixed(number)
+//returms a string representing the given number using fixed-point notion to the passed in argument
+
+const price = 242;
+const strPrice = price.toFixed(2);
+console.log(strPrice);
+//'242.00'
+console.log(typeof strPrice === 'string');
+//true
+const backToIntPrice = Number(strPrice);
+console.log(backToIntPrice);
+//242
+console.log(backToIntPrice === '242.00');
+//true
+console.log(backToIntPrice === 242.0);
+//true
+console.log(typeof backToIntPrice === 'number');
+//true
+console.log(strPrice === backToIntPrice);
+//false
