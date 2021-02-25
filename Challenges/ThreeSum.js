679c7781ba60d5c413daadbfc902f0bbe7de6165
Scott Campbell <stc9486@gmail.com>
2021-02-25 13:51:53 -0500

    chal

--- Challenges/ThreeSum.js
+++ Challenges/ThreeSum.js
@@ -1,45 +1,58 @@
-const threeSum = (nums) => {
-    const output = [];
+
+const threeSum = arr => {
+    const nums = arr.sort((a, b) => a - b);
+
+    const trips = [];
     
-    for (let i = 0; i < nums.length; i++) {
-      const first = nums[0];
-      for (let j = i + 1; j < nums.length; j++) {
-        const second = nums[j];
-        for (let k = i + 2; k < nums.length; k++) {
-          const third = nums[k]
-                
-          if (first + second + third === 0) {
-            const triple = [first, second, third];
-            console.log('triple', triple);
-                    
-            if (output.length === 0) output.push(triple)
-  
-                    
-            else {
-              let dupe = 0;
-  
-                for (let i = 0; i < output.length; i++) {
-                  const currArr = output[i];
-  
-                  //console.log('curr', currArr);
-  
-                  for (let j = 0; j < triple.length; j++) {
-  
-                    const tripNum = triple[j];
-                          
-                    if (currArr.includes(tripNum) === true) {
-                      dupe += 1;   
-                    }
-                    
-                    if (dupe < triple.length) output.push(triple);
-                }
-              }
-            }      
-          }
-        }
-      }
+    if (nums[0] > 0) return trips;
+    
+    for (let indexA = 0; indexA < nums.length - 2; indexA++) {  
+        
+      const currA = nums[indexA];
+        
+      if (currA === nums[indexA - 1]) continue;
+        
+      let indexB = indexA + 1;
+      let indexC = nums.length - 1;
+        
+      while (indexB < indexC)  {
+
+        const currB = nums[indexB];
+        const currC = nums[indexC];
+
+        const trio = [currA, currB, currC];
+        const trioSum = trio.reduce((acc, curr) => acc += curr);
+
+        if (trioSum === 0) {
+          trips.push(trio);
+          indexB++;
+          indexC--;
+        };
+                                                          
+        
+        if ((currA + currB + currC) > 0) {
+          while (nums[indexC - 1] === currC) {indexC--};
+          indexC--
+        }        
+        
+        if ((currA + currB + currC) < 0) {
+          while (nums[indexB + 1] === currB) {indexB++};
+          indexB++
+        }                                                       
+      }   
     }
-    //return output;
-  };
+    
+    const hashMap = {}
+
+    trips.forEach(arr => hashMap[arr.join("|")] = arr);
+
+    const res = Object.keys(hashMap).map(hashVal => hashMap[hashVal]);
+    
+  //or  
+  //const res = Array.from(new Set(input.map(JSON.stringify)), JSON.parse)
+
+    return res;
+};
   
-  console.log(threeSum([-1,0,1,2,-1,-4]))
+console.log(threeSum([-1,0,1,2,-1,-4]))
+//[[-1, -1, 2], [-1, 0, 1]]
