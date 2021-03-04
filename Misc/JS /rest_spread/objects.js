a5e01b48ea2775f73d079ab343d6e90c0365d5a0
Scott Campbell <stc9486@gmail.com>
2021-03-04 00:17:59 -0500

    spread

--- Misc/JS /rest_spread/objects.js
+++ Misc/JS /rest_spread/objects.js
@@ -1,4 +1,11 @@
-//* Spread first
+//SPREAD
+
+const villain = {
+	alias: null,
+	nemesis: null,
+	danger: null,
+};
+
 const Joker = { ...villain, alias: 'Joker', nemesis: 'Batman', danger: 10 };
 const Penguin = { ...villain, alias: 'Penguin', nemesis: 'Batman' };
 
@@ -6,3 +13,11 @@ console.log(Joker);
 //{ alias: 'Joker', nemesis: 'Batman', danger: 10 }
 console.log(Penguin);
 //{ alias: 'Penguin', nemesis: 'Batman', danger: 'N/A' }
+
+const chillJoker = { ...Joker, danger: 0 };
+console.log(chillJoker);
+//{ alias: 'Joker', nemesis: 'Batman', danger: 10 }
+
+const copyVillain = { ...villain };
+console.log(copyVillain);
+// {alias: null, nemesis: null, danger: null}
