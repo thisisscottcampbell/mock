248dfe7822f5a5756e6dc7f027261e37b24d72c9
Scott Campbell <stc9486@gmail.com>
2021-03-08 14:45:44 -0500

    Classes

---
+++ Misc/JS /Class.js
@@ -0,0 +1,31 @@
+class Cat {
+	constructor(name, color, gender) {
+		this.name = name;
+		this.color = color;
+		this.gender = gender;
+	}
+	getName() {
+		return this.name;
+	}
+	setName(newName) {
+		return (this.name = newName);
+	}
+}
+
+const Garfield = new Cat('Garfield', 'orange', 'M');
+const Buddy = new Cat('Buddy', 'orange', 'M');
+const Felix = new Cat('Felix', 'tuxedo', 'M');
+
+console.log(Garfield);
+//Cat { name: 'Garfield', color: 'orange', gender: 'M' }
+console.log(Buddy);
+//Cat { name: 'Buddy', color: 'orange', gender: 'M' }
+console.log(Felix);
+//Cat { name: 'Felix', color: 'tuxedo', gender: 'M' }
+
+console.log(Felix.getName());
+//Felix
+
+Felix.setName('Sylvester');
+
+console.log(Felix.getName());
