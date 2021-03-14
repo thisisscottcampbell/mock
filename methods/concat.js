8729f1854b256ac8966c4ccea3062719389509c7
Scott Campbell <stc9486@gmail.com>
2021-03-13 20:25:09 -0500

    concat

---
+++ methods/concat.js
@@ -0,0 +1,8 @@
+//method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+
+const cats = ['Felix', 'Garfield', 'Cheshire'];
+const dogs = ['Undergod', 'Snoopy', 'Pluto'];
+const catsAndDogs = cats.concat(dogs);
+
+console.log(catsAndDogs);
+// ['Felix', 'Garfield', 'Cheshire', 'Undergod', 'Snoopy', 'Pluto']
