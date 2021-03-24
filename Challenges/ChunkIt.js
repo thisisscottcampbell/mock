9801efff37e60b4b01ecab7a51503e77befcf236
Scott Campbell <thisisscottcampbell@gmail.com>
2021-03-23 23:16:20 -0400

    chal

--- Challenges/ChunkIt.js
+++ Challenges/ChunkIt.js
@@ -1,5 +1,6 @@
 // Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
 
+//RECURSIVE
 const chunkItUp = (arr, size) => {
 	const chunksArr = [];
 
@@ -31,10 +32,37 @@ const chunkItUp = (arr, size) => {
 	return chunksArr;
 };
 
-//chunkItUp(["a", "b", "c", "d"], 2) //-> [["a", "b"], ["c", "d"]].
-//chunkItUp([0, 1, 2, 3, 4, 5], 3) //-> [[0, 1, 2], [3, 4, 5]].
-//chunkItUp([0, 1, 2, 3, 4, 5], 2) //-> [[0, 1], [2, 3], [4, 5]].
-//chunkItUp([0, 1, 2, 3, 4, 5], 4) //-> [[0, 1, 2, 3], [4, 5]].
-//chunkItUp([0, 1, 2, 3, 4, 5, 6], 3) //-> [[0, 1, 2], [3, 4, 5], [6]].
-//chunkItUp([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) //-> [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
-//chunkItUp([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) //-> [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
+//ITERATIVE
+function chunkItUp(array, size) {
+	const chunkArr = [];
+
+	for (i = 0; i < array.length; ) {
+		const chunk = [];
+
+		while (chunk.length < size && i < array.length) {
+			const curr = array[i];
+
+			chunk.push(curr);
+			i += 1;
+		}
+
+		chunkArr.push(chunk);
+	}
+
+	return chunkArr;
+}
+
+// console.log(chunkItUp(['a', 'b', 'c', 'd'], 2));
+//-> [["a", "b"], ["c", "d"]].
+//chunkItUp([0, 1, 2, 3, 4, 5], 3)
+//-> [[0, 1, 2], [3, 4, 5]].
+//chunkItUp([0, 1, 2, 3, 4, 5], 2)
+//-> [[0, 1], [2, 3], [4, 5]].
+//chunkItUp([0, 1, 2, 3, 4, 5], 4)
+//-> [[0, 1, 2, 3], [4, 5]].
+//chunkItUp([0, 1, 2, 3, 4, 5, 6], 3)
+//-> [[0, 1, 2], [3, 4, 5], [6]].
+//chunkItUp([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)
+//-> [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+// console.log(chunkItUp([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
+//-> [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
