cc729d845eec41122769d503330fdd2119c63968
Scott Campbell <thisisscottcampbell@gmail.com>
2021-03-23 23:10:03 -0400

    dbl ll, ds

---
+++ Data Structures/Doubly Linked Lists/DoublyLinkedLists.js
@@ -0,0 +1,16 @@
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.next = null;
+		this.next = null;
+	}
+}
+
+class DoubleList {
+	constructor(val) {
+		const node = new Node(value);
+		this.head = node;
+		this.tail = this.head;
+		value ? (this.length = 1) : (this.length = 0);
+	}
+}
