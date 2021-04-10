f28efcb9525d4a2d5872219f1ac68a4eaee7d9ac
Scott Campbell <thisisscottcampbell@gmail.com>
2021-04-10 13:34:27 -0400

    math

--- methods/Math....js
+++ methods/Math....js
@@ -1,49 +1,57 @@
 //Math.floor
-  //rounds num down to whole interger
-(Math.floor(1.8))
-  //--> 1
-(Math.floor(3.3))
-  // --> 3
+//rounds num down to whole interger
+Math.floor(1.8)(
+	//--> 1
+	Math.floor(3.3)
+)(
+	// --> 3
 
-//Math.ceil
-  //rounds num up to whole interger
-(Math.ceil(1.5))
-//--> 2
-(Math.ceil(3.3))
-// --> 4
+	//Math.ceil
+	//rounds num up to whole interger
+	Math.ceil(1.5)
+)(
+	//--> 2
+	Math.ceil(3.3)
+)(
+	// --> 4
 
-
-//Math.random
-  //generates random number
-(Math.random());
-  // --> [some random number between 0 - 1]
-  Math.floor(Math.random() * 10) 
-    // random number between 1 and 10
-  const getNumberBetweenNums = (min, max) => {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min) + min);
-    }
+	//Math.random
+	//generates random number
+	Math.random()
+);
+// --> [some random number between 0 - 1]
+Math.floor(Math.random() * 10);
+// random number between 1 and 10
+const getNumberBetweenNums = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min) + min);
+};
 
 //Random between 1 and 100
-    Math.floor(Math.random() * 100)
-     
-//Math.sign
-  //returns 1 if number is positive; 0 if a num is zero; and -1 if a number is negative;
-(Math.sign(-5));
-  // --> -1
+Math.floor(Math.random() * 100)(
+	//Math.sign
+	//returns 1 if number is positive; 0 if a num is zero; and -1 if a number is negative;
+	Math.sign(-5)
+);
+// --> -1
 
 //Math.max
-  //returns highest number
-Math.max(1,4,2);
+//returns highest number
+Math.max(1, 4, 2);
 // --> 4
 
 //Math.min
-  //returns lowest number
-Math.min(1,4,2);
-  // --> 1
+//returns lowest number
+Math.min(1, 4, 2);
+// --> 1
 
 //Math.pow
-  //return ar1 to the power of arg2
-Math.pow(2,4)
-  // --> 16
+//return ar1 to the power of arg2
+Math.pow(2, 4);
+// --> 16
+
+//Math.abs
+//returns the absolute value
+Math.abs(-3);
+// --> 3
