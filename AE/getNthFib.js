17f636d086708fe5912b2fec014f0756686cc0ab
Scott Campbell <thisisscottcampbell@gmail.com>
2021-04-13 17:27:41 -0400

    ae recur

--- AE/getNthFib.js
+++ AE/getNthFib.js
@@ -1,3 +1,10 @@
+//SOLUTION A
+const getNthFib = (n) => {
+	if (n === 2) return 1;
+	else if (n === 1) return 0;
+	else return getNthFib(n - 1) + getNthFib(n - 2);
+};
+
 //SOLUTION B
 function getNthFib(n) {
 	const sequence = [];
