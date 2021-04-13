def8b8d502f9eb2676b1f344cc123d6e994161c7
Scott Campbell <thisisscottcampbell@gmail.com>
2021-04-13 19:16:27 -0400

    ok

--- Challenges/Recursion/ProductSum.js
+++ Challenges/Recursion/ProductSum.js
@@ -1,4 +1,4 @@
-function productSum(array) {
+const productSum = (array) => {
 	let sum = 0;
 
 	const process = (arr, lvl = 1) => {
@@ -12,5 +12,19 @@ function productSum(array) {
 	};
 
 	process(array);
+	return sum;
+};
+
+//VS
+
+function productSum(array, multiplier = 1) {
+	let sum = 0;
+	for (let element of array) {
+		if (Array.isArray(element)) {
+			sum += productSum(element, multiplier + 1) * multiplier;
+		} else {
+			sum += element * multiplier;
+		}
+	}
 	return sum;
 }
