ccb08267cd3c1d3277c9357cf5a701624729e9e7
Scott Campbell <thisisscottcampbell@gmail.com>
2021-04-14 12:05:45 -0400

    js

--- Misc/JS /For In vs For Of/ForInVSForOf.js
+++ Misc/JS /For In vs For Of/ForInVSForOf.js
@@ -1,31 +1,43 @@
-const cats = ['buddy', 'truman', 'scooby'];
+const catsArr = ['buddy', 'truman', 'scooby'];
 
-const heroes = {
+const heroesObj = {
 	batman: 'Bruce Wayne',
 	superman: 'Clark Kent',
 	ironman: 'Tony Stark',
 };
 
+const str = 'str';
+
 //FOR IN
 //returns a list of keys on the object being iterated
-for (const cat in cats) {
+for (const cat in catsArr) {
 	console.log(cat);
 }
-// "0", "1", "2",
+// "0", "1", "2"
 
-for (const hero in heroes) {
+for (const char in str) {
+	console.log(char);
+}
+// "0", "1", "2"
+
+for (const hero in heroesObj) {
 	console.log(hero);
 }
 //"batman", "superman", "ironman"
 
 //FOR OF
 //returns a list of values of the properites of the objet being iterated
-for (const cat of cats) {
+for (const cat of catsArr) {
 	console.log(cat);
 }
 // "buddy", "truman", "scooby"
 
-for (const hero of heroes) {
+for (const char of str) {
+	console.log(char);
+}
+//"s", "t", "r"
+
+for (const hero of heroesObj) {
 	console.log(hero);
 }
-// Not Iterable
+//Not Iterable
