e4fe8eb8e22368910d97b90a13d10fd5a9b1e7dc
Scott Campbell <thisisscottcampbell@gmail.com>
2021-05-04 16:19:41 -0400

    ae bst cons

--- AE/BST/_insert_remove_contains.js
+++ AE/BST/_insert_remove_contains.js
@@ -15,11 +15,42 @@ class BST {
 		}
 		return this;
 	}
+
+	contains(value) {
+		if (this.value === value) return true;
+
+		if (value < this.value) {
+			if (this.left === null) return false;
+			return this.left.contains(value);
+		} else {
+			if (this.right === null) return false;
+			return this.right.contains(value);
+		}
+	}
+
+	__contains(value) {
+		if (value < this.value) {
+			if (this.left === null) return false;
+			return this.left.contains(value);
+		} else if (value > this.value) {
+			if (this.right === null) return false;
+			return this.right.contains(value);
+		} else return true;
+	}
 }
 
 let tree = new BST(10);
 tree.insert(4);
 tree.insert(12);
-console.log(tree);
+tree.insert(7);
 tree.insert(34);
 tree.insert(11);
+console.log(tree);
+let doesContain = tree.contains(11);
+console.log(doesContain);
+doesContain = tree.__contains(11);
+console.log(doesContain);
+doesContain = tree.contains(13);
+console.log(doesContain);
+doesContain = tree.__contains(13);
+console.log(doesContain);
