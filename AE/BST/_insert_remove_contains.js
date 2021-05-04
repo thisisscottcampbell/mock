a91d0759072c0fd1d3be2c2fca5d07d2f8afe695
Scott Campbell <thisisscottcampbell@gmail.com>
2021-05-04 16:03:25 -0400

    edit pre

---
+++ AE/BST/_insert_remove_contains.js
@@ -0,0 +1,25 @@
+class BST {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+
+	insert(value) {
+		if (value < this.value) {
+			if (this.left === null) this.left = new BST(value);
+			else this.left.insert(value);
+		} else {
+			if (this.right === null) this.right = new BST(value);
+			else this.right.insert(value);
+		}
+		return this;
+	}
+}
+
+let tree = new BST(10);
+tree.insert(4);
+tree.insert(12);
+console.log(tree);
+tree.insert(34);
+tree.insert(11);
