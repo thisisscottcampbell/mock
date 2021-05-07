32d67980aa14adc063c9c5c7e1384c7c1194a954
Scott Campbell <thisisscottcampbell@gmail.com>
2021-05-07 15:35:48 -0400

    post

--- AE/BST/Depth First Searches/PostOrder.js
+++ AE/BST/Depth First Searches/PostOrder.js
@@ -1,8 +1,45 @@
-function postOrderTraverse(tree, array) {
+class BST {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+
+	insert(value) {
+		if (value < this.value) {
+			if (this.left === null) this.left = new BST(value);
+			else this.left.insert(value);
+		} else {
+			if (this.right === null) this.right = new BST(value);
+			else this.right.insert(value);
+		}
+		return this;
+	}
+}
+
+let tree = new BST(10);
+tree.insert(15);
+tree.insert(22);
+tree.insert(5);
+tree.insert(6);
+tree.insert(2);
+tree.insert(1);
+
+const postOrderTraverse = (tree, array = []) => {
+	console.log(array);
+
+	if (!tree) console.log('done here');
+
 	if (tree) {
+		console.log('curr val', tree.value);
+
 		postOrderTraverse(tree.left, array);
 		postOrderTraverse(tree.right, array);
 		array.push(tree.value);
 	}
+
 	return array;
-}
+};
+
+const postOrder = postOrderTraverse(tree);
+console.log('postOrder =', postOrder);
