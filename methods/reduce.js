d5e44887e39ae48f14c72ba79c9db05baeb5e2d6
Scott Campbell <thisisscottcampbell@gmail.com>
2021-05-17 22:13:21 -0400

    series clean up

--- methods/reduce.js
+++ methods/reduce.js
@@ -8,63 +8,60 @@
 // *** whatever is returned from the callback function becomes the new value of the accumulator
 // *** so acc is what we return
 
-
 // array.reduce(accumulator, nextValue, index, array) {
-  //callback functionality
+//callback functionality
 // }, optional second param)
-
 
 //acc = first value in the array or optional second param
 //nextValue = second value in the array or first if the optional second param is passed
 
 //get total
-const sum = nums.reduce((acc, next) => {
-  return acc + next
-}, /*opt*/)
+const sum = nums.reduce(
+	(acc, next) => {
+		return acc + next;
+	} /*opt*/
+);
 
-const arrayOfNames = [ 'player1', 'player2', 'player3' ]
-names.reduce((acc, next) =>{
-  return acc += '' next;
-}, 'The team members are')
+const cats = ['Felix', 'Garfield', 'Sylvester'];
 
-//returns, 'The team members are player1 player2 player3'
+cats.reduce((acc, next) => {
+	return (acc += `${next}`);
+}, 'The team members are');
 
+//'Felix Garfield Sylvester'
 
 //check dupes, or count occurences by created obj ... TALLY
 
-const array = [5,4,1,4,5]
-
-array.reduce((acc, next) =>{
-  if(next in acc) {
-    acc[next]++;
-  } else {
-    acc[next] = 1
-  }
-  return acc
-},{})
-
-----OOORRRR-----
+const array = [5, 4, 1, 4, 5];
 
 array.reduce((acc, next) => {
-  acc[next] ? acc[next]++ : acc[next] = 1;
-return acc
-},{})
+	if (next in acc) {
+		acc[next]++;
+	} else {
+		acc[next] = 1;
+	}
+	return acc;
+}, {});
+
+----OOORRRR---- -
+	array.reduce((acc, next) => {
+		acc[next] ? acc[next]++ : (acc[next] = 1);
+		return acc;
+	}, {});
 
 // returns {5:2, 4:2, 1:1}
 
-
 //Count the values in an Array
-const orders = [
-  { amount: 25 },
-  { amount: 25 },
-  { amount: 25 },
-  { amount: 25 }
-]
+const orders = [{ amount: 25 }, { amount: 25 }, { amount: 25 }, { amount: 25 }];
 
-const totalAmount = orders.reduce(sum, order => {
-  console.log (sum, order)
-  return sum + order.amount
-}, 0)
+const totalAmount = orders.reduce(
+	sum,
+	(order) => {
+		console.log(sum, order);
+		return sum + order.amount;
+	},
+	0
+);
 // log 1: 0, 25
 // log 2: 25, 50
 // log 3: 50, 75
@@ -73,31 +70,30 @@ const totalAmount = orders.reduce(sum, order => {
 //DEDUPE
 
 deDupeArray.reduce((accArray, item) => {
-  return accArray.includes(item) ? accArray : [...accArray, item]
-}, [])
+	return accArray.includes(item) ? accArray : [...accArray, item];
+}, []);
 
-//accArray; 
-  //is my acculator and it is set to the inital value of an empty array; it is what I am reducing deDeupeArray to
+//accArray;
+//is my acculator and it is set to the inital value of an empty array; it is what I am reducing deDeupeArray to
 
-//item: 
-  //is the item in deDupeArray that I am evaluating
+//item:
+//is the item in deDupeArray that I am evaluating
 
 //accArray.includes(item)
-  //this is the condition against which I am evaluating 'item': is 'item' in accArray?
+//this is the condition against which I am evaluating 'item': is 'item' in accArray?
 
 // accArray : [...accArray, item]
-  // if yes --> then accArray remains as is
-  // if no --> then 'item' is added to accArray
+// if yes --> then accArray remains as is
+// if no --> then 'item' is added to accArray
 
+//GET HIGHEST VALUE IN ARRAY
 
-  //GET HIGHEST VALUE IN ARRAY
+const nums = [1, 2, 4, 5, 6, 7, 3, 9, 3];
 
-  const nums = [1,2,4,5,6,7,3,9,3]
-
-  array.reduce((acc, item) => {
-    return acc > item ? acc : item
-  })
+array.reduce((acc, item) => {
+	return acc > item ? acc : item;
+});
 
 //Flatten a multi-dimensional array
 
-  const flatArray = array.reduce((acc, item) => acc.concat(item));
+const flatArray = array.reduce((acc, item) => acc.concat(item));
